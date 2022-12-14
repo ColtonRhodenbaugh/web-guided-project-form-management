30e1db918f1c7de920a60b40c3448c8be3416090
Colton Rhodenbaugh <coltonrhodenbaugh@Coltons-Air.hsd1.il.comcast.net>
2022-12-13 18:19:12 -0600

    all done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import { render } from 'react-dom'
 // 👉 App contains a more sophisticated form we'll flesh out later
 import App from './components/App'
@@ -9,15 +10,60 @@ const petsList = [
   { petName: 'Tweetie', petType: 'canary' },
   { petName: 'Goldie', petType: 'fish' },
 ]
+const initialFormValues = { petName: "", petType: "" };
 
 function SimpleForm() {
-  return <div>Ready to start GP!</div>
-}
+  const [pets, setPets] = useState(petsList);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const change = (evt) => {
+    const { name, value } = evt.target;
+
+    setFormValues({...formValues, [name]: value})
+  }
+
+  const submit = (evt) => {
+    evt.preventDefault();
+    const newPet ={
+      petName: formValues.petName.trim(),
+      petType: formValues.petType.trim(),
+    };
+    setPets(pets.concat(newPet));
+    setFormValues(initialFormValues);
+  }
+
+  return( 
+  <div className="container">
+    <h1>Simple Form App</h1>
+    { pets.map((pet, idx) => {
+      return(
+        <div key={idx}>
+           {pet.petName} is a {pet.petType}
+        </div>
+      )
+    })}
+    <form onSubmit={submit}>
+      <input 
+        name="petName"
+        type="text"
+        value={formValues.petName}
+        onChange={change}
+      />
+      <input 
+        name="petType"
+        type="text"
+        value={formValues.petType}
+        onChange={change}
+      />
+      <input type="submit" value="Create a pet!" />
+    </form>
+  </div>
+)}
 
 render(
   <>
-    <SimpleForm />
-    {/* <App /> */}
+   <SimpleForm />
+     <App /> 
   </>
   , document.querySelector('#root')
 )
